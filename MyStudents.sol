a01ac1eccd1c892a73f12fae94d562b2c6341b93
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-01-21 20:17:19 +0545

    Create MyStudents.sol
    In this contract, the instructor records the roll no, name and marks of students in a classroom
    and the students(or anyone) can see their marks by using their roll nos.

---
+++ MyStudents.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.1;
+
+contract MyStudents {
+    struct Student {
+        uint roll;
+        string firstName;
+        string lastName;
+        uint marks;
+    }
+    
+    address owner;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    mapping (uint => Student) public students;
+    
+    uint public stdCount;
+    
+    function addPerson(string memory _firstName, string memory _lastName, uint _marks) public onlyOwner {
+        incrementCount();
+        students[stdCount] = Student(stdCount, _firstName, _lastName, _marks); 
+    } 
+    
+    function incrementCount() internal {
+        stdCount += 1;
+    }
+
+}
